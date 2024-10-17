8e92b7130a987c33e317b36a55da37d772228cfe
LADR100 <145069751+LADR100@users.noreply.github.com>
2024-10-17 19:02:37 -0400

    footer actualizado 1.1

--- lib/presentation/widgets/main_footer.dart
+++ lib/presentation/widgets/main_footer.dart
@@ -37,6 +37,7 @@ class FooterWidget extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Container(
       color: Colors.grey[850],
       padding: EdgeInsets.symmetric(vertical: 100, horizontal: 20),
@@ -45,26 +46,33 @@ class FooterWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     children: [
-                      Icon(Icons.location_city, color: Colors.white),
-                      SizedBox(width: 8),
+                      Image.asset(
+                        'assets/alcaldia-de-maracaibo.png', // Ruta de la imagen
+                        width: 150, // Ajusta el tamaño de la imagen
+                        height: 150,
+                      ),
+                      SizedBox(width: 10),
                       Text(
                         'FOMUTUR',
                         style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            fontFamily: 'AlcaldiaFonts'),
                       ),
                     ],
                   ),
                   SizedBox(height: 10),
                   Text(
                     'En nuestra alcaldía, la transparencia es\nnuestra prioridad. Accede a la información\npública de manera clara y accesible.',
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: Colors.white, fontSize: 20, fontFamily: 'AlcaldiaFonts'),
                   ),
                 ],
               ),
@@ -74,29 +82,35 @@ class FooterWidget extends StatelessWidget {
                   Text(
                     'Contacto',
                     style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                        color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20, fontFamily: 'AlcaldiaFonts'),
                   ),
                   SizedBox(height: 5),
                   Text(
                     'Horario:\nLunes a viernes 8:00 a.m. a 4:00 p.m.',
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: Colors.white, fontSize: 20, fontFamily: 'AlcaldiaFonts'),
                   ),
                   SizedBox(height: 5),
                   Text(
                     'Dirección:\nAvenida 4 con Calle 96,\nMaracaibo, Venezuela',
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: Colors.white, fontSize: 20, fontFamily: 'AlcaldiaFonts'),
                   ),
                   SizedBox(height: 5),
-                  Row(
+                  InkWell(
+                   onTap: () {
+                   // Aquí se define el enlace de WhatsApp
+                   _launchURL('https://wa.me/584127632888'); // Cambia este número según sea necesario
+                  },
+                  child: Row(
                     children: [
                       Icon(FontAwesomeIcons.whatsapp, color: Colors.green),
                       SizedBox(width: 8),
                       Text(
                         'WhatsApp',
-                        style: TextStyle(color: Colors.white),
-                      )
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20, fontFamily: 'AlcaldiaFonts'),
+                      ),
                     ],
-                  )
+                  ),
+                ),
                 ],
               ),
               Column(
@@ -105,7 +119,7 @@ class FooterWidget extends StatelessWidget {
                   Text(
                     'Síguenos en nuestras redes sociales',
                     style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                        color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20, fontFamily: 'AlcaldiaFonts'),
                   ),
                   SizedBox(height: 10),
                   Row(
@@ -114,35 +128,43 @@ class FooterWidget extends StatelessWidget {
                         icon: Icon(FontAwesomeIcons.whatsapp),
                         color: Colors.white,
                         onPressed: () {
-                          _launchURL("https://wa.me/584127632888");
+                          _launchURL("https://wa.me/584127334000");
                         },
                       ),
                       IconButton(
                         icon: Icon(FontAwesomeIcons.instagram),
                         color: Colors.white,
                         onPressed: () {
-                          _launchURL("https://wa.me/584127632888");
+                          _launchURL("https://www.instagram.com/mcboalcaldia/");
                         },
                       ),
                       IconButton(
                         icon: Icon(FontAwesomeIcons.facebook),
                         color: Colors.white,
-                        onPressed: () {},
+                        onPressed: () {
+                          _launchURL("https://www.facebook.com/mcboalcaldia/");
+                        },
                       ),
                       IconButton(
                         icon: Icon(FontAwesomeIcons.twitter),
                         color: Colors.white,
-                        onPressed: () {},
+                        onPressed: () {
+                          _launchURL("https://www.twitter.com/mcboalcaldia/");
+                        },
                       ),
                       IconButton(
                         icon: Icon(FontAwesomeIcons.tiktok),
                         color: Colors.white,
-                        onPressed: () {},
+                        onPressed: () {
+                          _launchURL("https://www.tiktok.com/@mcboalcaldia/");
+                        },
                       ),
                       IconButton(
                         icon: Icon(FontAwesomeIcons.youtube),
                         color: Colors.white,
-                        onPressed: () {},
+                        onPressed: () {
+                          _launchURL("https://www.youtube.com/@mcboalcaldia/");
+                        },
                       ),
                     ],
                   )
