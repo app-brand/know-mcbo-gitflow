18e1fa07c85b62b5c6d36ec1bd807bbd95a00980
Carlos <data4if@gmail.com>
2024-10-23 15:25:23 -0400

    mapa - changes

--- lib/presentation/core/directions_repository.dart
+++ lib/presentation/core/directions_repository.dart
@@ -1,73 +1,63 @@
+import 'package:dio/dio.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:know_my_city/presentation/core/directions_model.dart';
+import 'package:know_my_city/presentation/core/.env.dart';
 
-/* class DirectionsRepository {
-  static const String _baseUrl = 'https://maps.googleapis.com/maps/api/directions/json?';
+class DirectionsRepository {
+  static const String _baseUrl =
+      'https://maps.googleapis.com/maps/api/directions/json';
 
   final Dio _dio;
 
   DirectionsRepository({Dio? dio}) : _dio = dio ?? Dio();
 
-  Future<Directions?> getDirections({
-    required LatLng tranvia, plaza, required LatLng origin, required LatLng destination
-    }) async {
-      final response = await _dio.get(
-        _baseUrl,
-        queryParameters: {
-          'origin': '${tranvia.latitude},${tranvia.longitude}',
-          'destination': '${plaza.latitude},${plaza.longitude}',
-          'key': googleAPIKey,
-        },
-      );
-
-      // revisar si la respuesta es correcta
-      if (response.statusCode == 200) {
-        return Directions.fromMap(response.data);
-      }
-      return null;
-    }
-} */
-class DirectionsRepository {
-  Future<List<List<LatLng>>> getMultipleDirections({
-    required List<Map<String, LatLng>> routes,
-  }) async {
-    const String proxyUrl = 'http://localhost:3000/directions';
+  Future<List<List<LatLng>>> getDirections(
+      {required List<Map<String, LatLng>> routes}) async {
+    print('calling - map dio');
+    Dio mi_dio = Dio();
     List<List<LatLng>> allPolylines = [];
-
     for (var route in routes) {
       final origin = route['origin']!;
       final destination = route['destination']!;
-      final String url =
-          '$proxyUrl?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&key=AIzaSyA0vsjSJXoisdpB-ouzVZuk0aCiEaievws';
-
+      // Los parámetros que necesitas para la API de Directions
+      /* Map<String, dynamic> queryParams = {
+        'origin':
+            origin.latitude.toString() + ',' + origin.longitude.toString(),
+        'destination': destination.latitude.toString() +
+            ',' +
+            destination.longitude.toString(),
+        'key': 'AIzaSyA0vsjSJXoisdpB-ouzVZuk0aCiEaievws',
+      }; */
       try {
-        final response = await http.get(Uri.parse(url));
+        String UrlMain =
+            'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude.toString()},${origin.longitude.toString()}&destination=${destination.latitude.toString()},${destination.longitude.toString()}&key=AIzaSyA0vsjSJXoisdpB-ouzVZuk0aCiEaievws';
+        print(UrlMain);
+        final Response response = await mi_dio.get(
+          UrlMain,
+          options: Options(headers: {'Access-Control-Allow-Origin': '*'}),
+        );
+        print(response.realUri);
+        print(response.data);
 
-        if (response.statusCode == 200) {
-          final data = json.decode(response.body);
-          /* print('Response data: $data'); */
+        // final data = json.decode(response.data);
 
-          final List<LatLng> polylinePoints = [];
+        final List<LatLng> polylinePoints = [];
 
-          if (data['routes'].isNotEmpty) {
-            final points = data['routes'][0]['overview_polyline']['points'];
-            polylinePoints.addAll(_decodePolyline(points));
-          }
-
-          /* print('Polyline points: $polylinePoints'); */
-          allPolylines.add(polylinePoints);
-        } else {
-          /* print('Failed to load directions: ${response.statusCode}'); */
-          throw Exception('Failed to load directions');
+        if (response.data['routes'].isNotEmpty) {
+          final points =
+              response.data['routes'][0]['overview_polyline']['points'];
+          polylinePoints.addAll(_decodePolyline(points));
         }
-      } catch (e) {
-        /* print('Error fetching directions: $e'); */
-        throw e;
+
+        /* print('Polyline points: $polylinePoints'); */
+        allPolylines.add(polylinePoints);
+      } on DioError catch (e) {
+        print(e);
       }
     }
-
     return allPolylines;
   }
 
@@ -75,6 +65,8 @@ class DirectionsRepository {
     PolylinePoints polylinePoints = PolylinePoints();
     List<PointLatLng> result = polylinePoints.decodePolyline(encoded);
 
-    return result.map((point) => LatLng(point.latitude, point.longitude)).toList();
+    return result
+        .map((point) => LatLng(point.latitude, point.longitude))
+        .toList();
   }
 }
