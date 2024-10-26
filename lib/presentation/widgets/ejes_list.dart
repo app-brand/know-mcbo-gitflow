b3befb394cdd75eb76eeb4ce14a865debd45d974
almejandrx <alehrafar@gmai.com>
2024-10-25 23:24:47 -0400

    Ajustes al eje idiosincrasia

--- lib/presentation/widgets/ejes_list.dart
+++ lib/presentation/widgets/ejes_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:know_my_city/presentation/core/app_theme.dart';
 import 'idiosincrasia_card.dart';  // Importa este archivo donde tengas el widget TravelOptionCard
 
 class TravelPageContent extends StatelessWidget {
@@ -9,24 +11,28 @@ class TravelPageContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(80.0),  // Reducimos el padding para hacer el layout más compacto
+        padding: const EdgeInsets.all(120.0),  // Reducimos el padding para hacer el layout más compacto
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Explore endless options with our service',
-              style: TextStyle(
-                fontSize: 24,  // Texto más pequeño
-                fontWeight: FontWeight.bold,
-              ),
+            Text(
+              'Explora sobre nuestras tradiciones y costumbres',
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                fontSize: 36,
+                color: AppTheme.greenAlcaldia,
+              ),                
+              ),              
             ),
             const SizedBox(height: 8),
             Text(
-              'Discover a myriad of choices available through our service, offering limitless possibilities for your exploration and enjoyment',
-              style: TextStyle(
-                fontSize: 14,  // Texto más pequeño
+              'Conoce más sobre la idiosincrasia de nuestra ciudad y sus habitantes.',
+              style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+                fontSize: 18,
                 color: Colors.grey[600],
-              ),
+                ),                
+              ),   
             ),
             const SizedBox(height: 16),
             StaggeredGrid.count(
@@ -37,30 +43,30 @@ class TravelPageContent extends StatelessWidget {
                 StaggeredGridTile.count(
                   crossAxisCellCount: 1,  // Ocupa 1 columna
                   mainAxisCellCount: 0.40,  // Ocupa 0.75 de una fila
-                  child: TravelOptionCard(
-                    imagePath: 'assets/images/axis/axi_1.jpeg',
-                    title: 'Comprehensive Travel Support',
+                  child: IdiosincrasiaCard(
+                    imagePath: 'assets/images/banner/tranvia.jpeg',
+                    title: 'Tranvía de Maracaibo',
                     description:
-                        '24/7 customer service to assist you before, during, and after your trip.',
+                        'Conoce la historia de este medio de transporte emblemático.',
                   ),
                 ),
                 StaggeredGridTile.count(
                   crossAxisCellCount: 1,  // Ocupa 1 columna
                   mainAxisCellCount: 0.80,  // Ocupa 1.5 filas
-                  child: TravelOptionCard(
-                    imagePath: 'assets/images/axis/axi_2.jpeg',
-                    title: 'Expert Travel Advice',
-                    description: 'Tips and guides to enhance your travel experience.',
+                  child: IdiosincrasiaCard(
+                    imagePath: 'assets/images/axis/gaita.jpeg',
+                    title: 'Gaitas',
+                    description: 'Descubre más sobre este género musical típico de la región.',
                   ),
                 ),
                 StaggeredGridTile.count(
                   crossAxisCellCount: 1,  // Ocupa 1 columna
                   mainAxisCellCount: 0.40,  // Ocupa 0.75 de una fila
-                  child: TravelOptionCard(
-                    imagePath: 'assets/images/axis/axi_3.jpeg',
-                    title: 'Diverse Destinations',
+                  child: IdiosincrasiaCard(
+                    imagePath: 'assets/images/axis/artesania.JPG',
+                    title: 'Artesanía',
                     description:
-                        'Access to a wide range of domestic and international locations.',
+                        'Conoce más sobre la artesanía zuliana y sus tradiciones.',
                   ),
                 ),
               ],
--- lib/presentation/widgets/idiosincrasia_card.dart
+++ lib/presentation/widgets/idiosincrasia_card.dart
@@ -1,72 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TravelOptionCard extends StatelessWidget {
+class IdiosincrasiaCard extends StatefulWidget {
   final String imagePath;
   final String title;
   final String description;
 
-  const TravelOptionCard({
+  const IdiosincrasiaCard({
+    Key? key,
     required this.imagePath,
     required this.title,
     required this.description,
-  });
+  }); 
+
+  @override
+  _IdiosincrasiaCardState createState() => _IdiosincrasiaCardState();
+}
+
+class _IdiosincrasiaCardState extends State<IdiosincrasiaCard> {
+  bool _isHovered = false;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16),
-      child: Stack(
-        children: [
-          Image.asset(
-            imagePath,
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.black.withOpacity(0.6), Colors.transparent],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: GoogleFonts.montserrat(
-                      textStyle: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Stack(
+            children: [
+              AnimatedScale(
+                scale: _isHovered ? 1.02 : 1.0,
+                duration: Duration(milliseconds: 300),
+                alignment: Alignment.center,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(widget.imagePath),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    description,
-                    style: GoogleFonts.montserrat(
-                      textStyle: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [Colors.black.withOpacity(0.8), Colors.transparent],
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(22.0),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.title,
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        widget.description,
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w200,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
