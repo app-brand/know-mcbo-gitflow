c29f190f4f7cae0590b3bc7915500444883650b4
almejandrx <alehrafar@gmai.com>
2024-11-04 11:34:52 -0400

    rutas listas, markers listos, footer nuevo

--- lib/presentation/widgets/header_custom.dart
+++ lib/presentation/widgets/header_custom.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:know_my_city/presentation/core/app_theme.dart';
 import 'package:know_my_city/presentation/core/router_core.dart';
+import 'package:know_my_city/presentation/core/state_core.dart';
 import 'package:know_my_city/presentation/dialogs/sign_in_dialog.dart';
+import 'package:provider/provider.dart';
 
 class HeaderCustom extends StatefulWidget {
   @override
@@ -13,11 +15,16 @@ class HeaderCustom extends StatefulWidget {
 class _HeaderCustomState extends State<HeaderCustom> {
   late String _randomImageUrl;
   bool _isVisible = false;
+  late StateCore stateCore;
 
   @override
   void initState() {
     super.initState();
     _randomImageUrl = _getRandomImageUrl();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      stateCore = Provider.of<StateCore>(context, listen: false);
+      stateCore.checkUserState();  
+    });
     // Iniciar la animación después de un pequeño retraso
     Future.delayed(Duration(milliseconds: 500), () {
       setState(() {
@@ -39,147 +46,171 @@ class _HeaderCustomState extends State<HeaderCustom> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        double width = MediaQuery.of(context).size.width; // Usar el ancho de la pantalla
-        double height = MediaQuery.of(context).size.height; // Usar la altura de la pantalla
-
-        return Stack(
-          children: [
-            // Imagen de fondo que ocupa toda la primera sección
-            Container(
-              width: width,
-              height: height, // Ocupa todo el alto disponible
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                      _randomImageUrl), // Ruta de la imagen aleatoria
-                  fit: BoxFit.cover,
-                  alignment: Alignment.topCenter,
-                  colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(0.15),
-                    BlendMode.hardLight,                                      
-                  ),
-                ),
-              ),
-            ),
-            // Navbar sobreposicionado
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.black.withOpacity(0.8),
-                      Colors.transparent,
-                    ],
-                  ),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      "images/banner/LOGOFOMUTURBLANCO.png", // Ruta de la imagen
-                      width: 200, // Tamaño fijo para el logo
-                      height: 100, // Tamaño fijo para el logo
+    final stateCore = Provider.of<StateCore>(context);
+    return Consumer<StateCore>(
+      builder: (context, coreState, child) {
+        return LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            double width = MediaQuery.of(context).size.width; // Usar el ancho de la pantalla
+            double height = MediaQuery.of(context).size.height; // Usar la altura de la pantalla
+            return Stack(
+              children: [
+                // Imagen de fondo que ocupa toda la primera sección
+                Container(
+                  width: width,
+                  height: height, // Ocupa todo el alto disponible
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          _randomImageUrl), // Ruta de la imagen aleatoria
+                      fit: BoxFit.cover,
+                      alignment: Alignment.topCenter,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.15),
+                        BlendMode.hardLight,                                      
+                      ),
                     ),
-                    // Opciones del navbar
-                    Row(
+                  ),
+                ),
+                // Navbar sobreposicionado
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.black.withOpacity(0.8),
+                          Colors.transparent,
+                        ],
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Inicio',
-                            style: GoogleFonts.montserrat(
-                              textStyle: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                /* fontWeight: FontWeight.w600, */
+                        Image.asset(
+                          "images/banner/LOGOFOMUTURBLANCO.png", // Ruta de la imagen
+                          width: 200, // Tamaño fijo para el logo
+                          height: 100, // Tamaño fijo para el logo
+                        ),
+                        // Opciones del navbar
+                        Row(
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Inicio',
+                                style: GoogleFonts.montserrat(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    /* fontWeight: FontWeight.w600, */
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        const SizedBox(width: 12), // Espacio entre los botones
-                        TextButton(
-                          onPressed: () {
-                            routerCore.push('/maps');
-                          },
-                          child: Text(
-                            'Mapa de Turista',
-                            style: GoogleFonts.montserrat(
-                              textStyle: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                /* fontWeight: FontWeight.w600, */
+                            const SizedBox(width: 12), // Espacio entre los botones
+                            TextButton(
+                              onPressed: () {
+                                if(stateCore.loggedIn){
+                                  routerCore.push('/maps');
+                                } else {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text('Acceso Denegado'),
+                                        content: const Text('Debes estar logueado para acceder a esta página.'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Cerrar'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                }                                                                    
+                              }, 
+                              child: Text(
+                                'Mapa de Turista',
+                                style: GoogleFonts.montserrat(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    /* fontWeight: FontWeight.w600, */
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        const SizedBox(width: 12), // Espacio entre los botones
-                        TextButton(
-                          onPressed: () {
-                            routerCore.push('/aboutus');
-                          },
-                          child: Text(
-                            'Nosotros',
-                            style: GoogleFonts.montserrat(
-                              textStyle: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                /* fontWeight: FontWeight.w600, */
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 12), // Espacio entre los botones
-                        TextButton(
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                return SignInDialog();
+                            const SizedBox(width: 12), // Espacio entre los botones
+                            TextButton(
+                              onPressed: () {
+                                routerCore.push('/aboutus');
                               },
-                            );
-                          },
-                          child: Text(
-                            'Perfil',
-                            style: GoogleFonts.montserrat(
-                              textStyle: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                /* fontWeight: FontWeight.w600, */
+                              child: Text(
+                                'Nosotros',
+                                style: GoogleFonts.montserrat(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    /* fontWeight: FontWeight.w600, */
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+                            const SizedBox(width: 12), // Espacio entre los botones
+                            TextButton(
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return SignInDialog();
+                                  },
+                                );
+                              },
+                              child: Text(
+                                'Perfil',
+                                style: GoogleFonts.montserrat(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    /* fontWeight: FontWeight.w600, */
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-            // Imagen animada centrada a la izquierda con fade-in
-            Positioned(
-              top: height/2.5, // Ajusta la posición vertical
-              left: 20, // Ajusta la posición horizontal
-              child: AnimatedOpacity(
-                opacity: _isVisible ? 1.0 : 0.0,
-                duration: const Duration(seconds: 1),
-                child: Image.asset(
-                  'assets/images/banner/logoconoce.png', // Ruta de la imagen
-                  width: 600, // Tamaño fijo para la imagen
-                  height: 300, // Tamaño fijo para la imagen
+                // Imagen animada centrada a la izquierda con fade-in
+                Positioned(
+                  top: height/2.5, // Ajusta la posición vertical
+                  left: 20, // Ajusta la posición horizontal
+                  child: AnimatedOpacity(
+                    opacity: _isVisible ? 1.0 : 0.0,
+                    duration: const Duration(seconds: 1),
+                    child: Image.asset(
+                      'assets/images/banner/logoconoce.png', // Ruta de la imagen
+                      width: 600, // Tamaño fijo para la imagen
+                      height: 300, // Tamaño fijo para la imagen
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ],
+              ],
+            );
+          },
         );
-      },
-    );
+      }      
+    ); 
   }
 }
