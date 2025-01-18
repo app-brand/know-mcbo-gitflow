97566357d9b3e2d62ebec9bb3e9b8e42fdf8dd2e
Carlos <data4if@gmail.com>
2025-01-18 13:23:57 -0400

    18-01-25: Issue json

---
+++ lib/presentation-fixed/home_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class McboMain extends StatefulWidget {
+  const McboMain({super.key});
+
+  @override
+  State<McboMain> createState() => _McboMainState();
+}
+
+class _McboMainState extends State<McboMain> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
